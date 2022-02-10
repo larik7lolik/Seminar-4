e6aa267d2c1df073c0dbb302deb0554bd60d728e
LarisaZaripova <larik7lolik@gmail.com>
2022-02-10 22:10:20 +0300

    added Example 27

---
+++ Example_27/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 27.
+Console.Write("Введите число: ");
+ string str = Console.ReadLine();
+ int num = int.Parse(str);
+ int counter = 0;
+ while(num! = 0)
+ {
+     num = num / 10;
+     counter++;
+ }
+ Console.WriteLine(counter);
