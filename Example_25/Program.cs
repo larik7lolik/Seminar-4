9042cb963b9e62c1858c7a7a5314160ad5ac43c0
LarisaZaripova <larik7lolik@gmail.com>
2022-02-10 22:09:47 +0300

    added Example 25

---
+++ Example_25/Program.cs
@@ -0,0 +1,15 @@
+﻿// 
+Console.WriteLine("Ввести число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine("Ввести число: 1");
+int numA = 1;
+int result = 0;
+while(numA <= num )
+{
+    result = result + numA;
+    numA++;
+}
+Console.WriteLine("Сумма " + result);
+
+
+
