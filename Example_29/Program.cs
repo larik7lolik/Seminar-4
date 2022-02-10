18ffe4fa9836ef9fac4d5a3f91684c6a55937a00
LarisaZaripova <larik7lolik@gmail.com>
2022-02-10 22:02:21 +0300

    added Example 29

---
+++ Example_29/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 29
+/*Console.Write21("Введите число 1: ");
+int n = int.Parse(Console.ReadLine());
+int m = 1;
+for(int i = 1; i <= n; i++)
+{
+    m = n * i;
+}
+Console.WriteLine(m);*/
+
+
+int Factorial(int num)
+{
+  int m = 1;
+  for(int i = 1; i <= num; i++)
+  {
+     m = m * i;
+  }
+return m;
+}
+Console.Write("Введите число 1: ");
+int n = int.Parse(Console.ReadLine());
+int f = Factorial(n);
+Console.Write(f);
