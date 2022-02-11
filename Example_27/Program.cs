fec8c630d89b019eb85772f59cf9650d00f4d647
LarisaZaripova <larik7lolik@gmail.com>
2022-02-11 09:39:05 +0300

    added new  Example 27

--- Example_27/Program.cs
+++ Example_27/Program.cs
@@ -1,9 +1,10 @@
-﻿// Задача 27.
+﻿// Задача 27: Определить количество цифр в числе.
 Console.Write("Введите число: ");
  string str = Console.ReadLine();
  int num = int.Parse(str);
+ //Console.WriteLine(str.Length);
  int counter = 0;
- while(num! = 0)
+ while(num!= 0)
  {
      num = num / 10;
      counter++;
