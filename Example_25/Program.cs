64140387f33c54aabd0fcc87e1dbb7d410a58c90
LarisaZaripova <larik7lolik@gmail.com>
2022-02-11 09:18:28 +0300

    added new Example 25

--- Example_25/Program.cs
+++ Example_25/Program.cs
@@ -1,5 +1,5 @@
-﻿// 
-Console.WriteLine("Ввести число: ");
+﻿// Задача 25: Найти сумму чисел от 1 до А.
+/*Console.WriteLine("Ввести число: ");
 int num = int.Parse(Console.ReadLine());
 Console.WriteLine("Ввести число: 1");
 int numA = 1;
@@ -9,7 +9,15 @@ while(numA <= num )
     result = result + numA;
     numA++;
 }
-Console.WriteLine("Сумма " + result);
+Console.WriteLine("Сумма " + result);*/
 
-
-
+Console.Clear();
+Console.Write("Введите число: ");
+int A = int.Parse(Console.ReadLine());
+Console.Write("Число: ");
+int B = 0;
+for(int i = 1; i <= A; i++)
+{
+    B = B + i;
+}
+Console.Write(B);
